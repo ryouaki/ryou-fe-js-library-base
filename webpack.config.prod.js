a2f79bbf1b07ec3b50b921e1a52e27ca831e08e9
梁亚辉 <liangyahui@didiglobal.com>
2019-12-17 19:22:15 +0800

    update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ module.exports = {
     test: './src/index.js'
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'lib'),
     filename: '[name].js',
     libraryTarget: 'umd',
     library: '[name]'
